3f54fd2ca795cb1de7dd706d7eb28db281bdb3f1
mooon-elxna <kreinelina@gmail.com>
2025-08-20 20:38:02 +0200

    added data check in input

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
 function sendData() {
-    
-    const p = Number(document.getElementById("valueP").value);
-    const q = Number(document.getElementById("valueQ").value);
-    // imports the value of the input as numbers
-
-    let d = ((p / 2) ** 2 - q);
-    //calculates the value under the sqrt
-
-    if (d < 0) {
-        document.getElementById("output1").innerHTML = "Nicht lösbar";
+    const valueP = document.getElementById("valueP").value;
+    const valueQ = document.getElementById("valueQ").value;
+    if (valueP.trim() === "" || valueQ.trim() === "") {
+        document.getElementById("output1").innerHTML = "Bitte beide Werte eingeben";
         document.getElementById("output2").innerHTML = " ";
-        // checks if the value under the sqrt is less than zero
-        // if true writes error instead of answer
+        return;
     }
 
-    else {
-        let resultA = -p / 2 + Math.sqrt(d);
-        resultA = resultA.toFixed(3)
-        let resultB = -p / 2 - Math.sqrt(d);
-        resultB = resultB.toFixed(3) 
-        //calculates formula
-        //rounds up numbers to 3 numbers after decimal
-        //converts to strings
+    else{
 
-        document.getElementById("output1").innerHTML = "x<sub>1</sub> = " + resultA;
-        document.getElementById("output2").innerHTML = "x<sub>2</sub> = " + resultB;
-        //displays the answers down below
-        
+  
+        const p = Number(document.getElementById("valueP").value);
+        const q = Number(document.getElementById("valueQ").value);
+        // imports the value of the input as numbers
+
+        let d = ((p / 2) ** 2 - q);
+        //calculates the value under the sqrt
+
+        if (d < 0) {
+            document.getElementById("output1").innerHTML = "Nicht lösbar";
+            document.getElementById("output2").innerHTML = " ";
+            // checks if the value under the sqrt is less than zero
+            // if true writes error instead of answer
+        }
+
+        else {
+            let resultA = -p / 2 + Math.sqrt(d);
+            resultA = resultA.toFixed(3)
+            let resultB = -p / 2 - Math.sqrt(d);
+            resultB = resultB.toFixed(3) 
+            //calculates formula
+            //rounds up numbers to 3 numbers after decimal
+            //converts to strings
+
+            document.getElementById("output1").innerHTML = "x<sub>1</sub> = " + resultA;
+            document.getElementById("output2").innerHTML = "x<sub>2</sub> = " + resultB;
+            //displays the answers down below
+            
+        }
+
     }
 
         
